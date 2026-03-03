78740cd780855783415d1691bc6069495695e6a5
Marcos <marcdmelo2gmail.com>
2026-03-02 22:22:41 -0300

    Inicio do projeto sistema de cadastro

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Cadastro</title>
+</head>
+
+<body>
+    <header>
+        <h1>Sistema de Cadastro</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php?menu=home">Home</a></li>
+                <li><a href="index.php?menu=cadastro">Cadastro</a></li>
+                <li><a href="index.php?menu=lista">Lista de Cadastros</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
